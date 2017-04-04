207cd919299ed21a0092a5eaf23d8bb5765b5a6a
ksamualtolch <ksamualtolch@gmail.com>
2017-04-04 11:24:07 -0700

    Remove setInterval

--- assets/js/random.js
+++ assets/js/random.js
@@ -1,9 +1,9 @@
 var ul = document.getElementById("codirectors")
 
-// setInterval(function(){
+
 	for (var i = ul.children.length; i >= 0; i--) {
     	ul.appendChild(ul.children[Math.random() * i | 0]);
 		}	
+
+// setInterval(function(){
 	// }, 10000);
-
-
